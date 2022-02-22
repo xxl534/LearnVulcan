3e2355d837cd7c48c71575fcaaac5707d9672464
xiexuelong <xiexuelong534@163.com>
2022-02-22 18:57:54 +0800

    command

--- src/vk_engine.h
+++ src/vk_engine.h
@@ -4,9 +4,27 @@
 #pragma once
 
 #include <vk_types.h>
+#include <vector>
 
 class VulkanEngine {
 public:
+
+	VkInstance _instance;
+	VkDebugUtilsMessengerEXT _debug_messenger;
+	VkPhysicalDevice _chosenGPU;
+	VkDevice _device;
+	VkSurfaceKHR _surface;
+
+	VkSwapchainKHR _swapChain;
+	VkFormat _swapchainImageFormat;
+	std::vector<VkImage> _swapchainImages;
+	std::vector<VkImageView> _swapchainImageViews;
+
+	VkQueue _graphicsQueue;
+	uint32_t _graphicsQueueFamily;
+
+	VkCommandPool _commandPool;
+	VkCommandBuffer _mainCommandBuffer;
 
 	bool _isInitialized{ false };
 	int _frameNumber {0};
@@ -26,4 +44,10 @@ public:
 
 	//run main loop
 	void run();
+private:
+	void init_vulkan();
+
+	void init_swapchain();
+
+	void init_command();
 };
